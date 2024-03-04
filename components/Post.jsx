7e8053c1aa896df0d95aa1b33bfe82fd4fe7a09f
Timur <timengalytchev@mail.ru>
2024-03-04 18:58:35 +0300

    add some fixes

--- components/Post.jsx
+++ components/Post.jsx
@@ -31,14 +31,22 @@ const PostDate = styled.Text`
   margin-top: 2px;
 `;
 
+const truncateTitle = (str) => {
+  if (str.length > 50) {
+    return str.substring(0,50) + '...'
+  }
+
+  return str
+}
+
 export const Post = ({title, imageUrl, createdAt}) => {
     return <PostView>
         <PostImage
           source={{ uri: imageUrl}}
         />
         <PostDetais>
-          <PostTitle>{title}</PostTitle>
-          <PostDate>{createdAt}</PostDate>
+          <PostTitle>{truncateTitle(title)}</PostTitle>
+          <PostDate>{new Date(createdAt). toLocaleDateString()}</PostDate>
         </PostDetais>
         
       </PostView>
